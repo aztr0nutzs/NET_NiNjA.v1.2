1f07e1a049f9ab4f1f51ae1ac81b9863fc79fb00
aztr0nutzs <aztr0.nutzs7@gmail.com>
2026-01-20 20:20:56 -0500

    chore: update gradle versions and restore assets

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,6 @@ plugins {
   kotlin("android") version "1.9.22" apply false
   kotlin("plugin.serialization") version "1.9.22" apply false
 
-  id("com.android.application") version "8.2.2" apply false
-  id("com.android.library") version "8.2.2" apply false
+  id("com.android.application") version "8.13.2" apply false
+  id("com.android.library") version "8.13.2" apply false
 }
